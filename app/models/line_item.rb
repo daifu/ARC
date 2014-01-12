2b97be6798acaca759b9f89e9a0212cfb5b59ed7
daifu <daifu.ye@gmail.com>
2014-01-11 23:00:10 -0800

    fixed coupon bug

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -10,11 +10,11 @@ class LineItem < ActiveRecord::Base
   end
 
   def early_bird_amount
-    early_bird_coupon = self.event.coupons.where("code='EARLYBIRD'")
+    early_bird_coupon = self.event.coupons.where("code='EARLYBIRD'").first
     early_bird_coupon.apply_discount(self.amount)
   end
 
   def early_bird_coupon_id
-    12
+    self.event.coupons.where("code='EARLYBIRD'").pluck(:id).first
   end
 end
